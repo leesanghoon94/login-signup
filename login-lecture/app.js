55e0a9406e9c471199e3042dc8f839d742bb9427
leesanghoon94 <leesanghoon94@github.com>
2023-08-17 16:49:55 +0900

    http로 간단한 서버연습

--- login-lecture/app.js
+++ login-lecture/app.js
@@ -1,13 +1,28 @@
-const express = require("express");
-const app = express();
-
-app.listen(3000, function(){
-    console.log("서버 가동");
+const http = require("http");
+const app = http.createServer((req, res) => {
+    res.writeHead(200, {"content-Type": "text/html; charset=utf-8"})
+    
+    if(req.url === "/"){
+        res.end("여기는 루트 입니다.");
+    } else if(req.url === "/login"){
+        res.end("여기는 로그인 화면 입니다.")
+    };
 });
 
-app.get("/", (req,res)=>{
-    res.send("여기는 루트입니다.")
+app.listen(3001, ()=>{
+    console.log("http로 가동된 서버 입니다.");
 });
-app.get("/login",(req,res)=>{
-    res.send("여기는 로그인 화면 입니다")
-});
+
+// const express = require("express");
+// const app = express();
+
+// app.listen(3000, function(){
+//     console.log("서버 가동");
+// });
+
+// app.get("/", (req,res)=>{
+//     res.send("여기는 루트입니다.")
+// });
+// app.get("/login",(req,res)=>{
+//     res.send("여기는 로그인 화면 입니다")
+// });
