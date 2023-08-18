b67b1b633f8edeb8017dce8815d72dcd79fcdc2b
leesanghoon94 <leesanghoon94@github.com>
2023-08-18 17:40:43 +0900

    폴더 구조화

---
+++ app/app.js
@@ -0,0 +1,34 @@
+"use strict";
+//http 로 서버
+// const http = require("http");
+// const app = http.createServer((req, res) => {
+//     res.writeHead(200, {"content-Type": "text/html; charset=utf-8"})
+
+//     if(req.url === "/"){
+//         res.end("여기는 루트 입니다.");
+//     } else if(req.url === "/login"){
+//         res.end("여기는 로그인 화면 입니다.")
+//     };
+// });
+// app.listen(3001, ()=>{
+//     console.log("http로 가동된 서버 입니다.");
+// });
+
+ 
+//모듈
+const express = require("express");
+const app = express();
+
+
+//라우팅
+const home = require("./src/routes/home");
+
+//앱세팅
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+
+//use -> 미들웨어를 등록해주는 메서드.
+app.use("/", home); 
+app.use(express.static(`${__dirname}/src/public`))
+
+module.exports = app;
