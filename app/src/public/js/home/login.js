f4b622219d847075dd5bec4ce88459ee4a5e4f03
leesanghoon94 <leesanghoon94@github.com>
2023-08-19 02:46:26 +0900

    로그인 성공  경로생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -22,7 +22,17 @@ function login(){
     })
     .then((res) => res.json())
     //.then((res) => console.log(res); 파라미터값을 또 파라미터로 넘길때 생략해줄수있다.
-    .then(console.log);
+    .then((res) => {
+        if(res.success) {
+            location.href = "/";
+        } else {
+            alert(res.message);
+        }
+    })
+    .catch((err) => {
+        console.error("로그인중 에러발생");
+        // console.error(new Error("로그인중 에러발생"));
+    })
   
     
 };
