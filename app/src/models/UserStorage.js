43894561d633eacd24aa441be3757b19557816d1
leesanghoon94 <leesanghoon94@github.com>
2023-08-23 02:17:39 +0900

    aws rds로 회원가입 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -3,38 +3,15 @@
 const db = require("../config/db");
 
 class UserStorage {
-//코딩문화 프라이빗한 변수나 메서드는 상단에
-    static #getUsersInfo(data, id) {
-        const users =JSON.parse(data);
-        const idx = users.id.indexOf(id);
-        const userKeys = Object.keys(users);
-        const userInfo = userKeys.reduce((newUsers, info) => { 
-            newUsers[info] = users[info][idx];
-            return newUsers;
-        }, {});
-        
-        return userInfo;
-    }
-    static #getUsers(data, isAll, fields){
-        const users = JSON.parse(data);
-        if(isAll) return users;
-        const newUsers = fields.reduce((newUsers, field) => {
-            if(users.hasOwnProperty(field)){
-                newUsers[field] = users[field];
-            }
-            return newUsers;
-        }, {});
-        return newUsers;
-    }
-
     
-    static getUsers(isAll, ...fields) {}
+    
 
     static getUsersInfo(id) {
        return new Promise((resolve, reject) => {
-            db.query("SELECT * FROM users WHERE id = ?", [id], (err, data) => {
+            const query = "SELECT * FROM users WHERE id = ?;";
+            db.query(query, [id], (err, data) => {
                 if(err) {
-                    reject(err);
+                    reject(`${err}`);
                 } else {
                     console.log(data[0]);
                 resolve(data[0]);
@@ -45,7 +22,19 @@ class UserStorage {
         
         }
 
-    static async save(userInfo) {}
+    static async save(userInfo) {
+        return new Promise((resolve, reject) => {
+            const query = "insert into users (id, name, password) value(?, ?, ?);";
+            db.query(
+                query, 
+                [userInfo.id, userInfo.name, userInfo.password], 
+                (err) => {
+                if(err) reject(`${err}`);
+                resolve({success: true});
+                
+            });
+        });
+    }
 };
 
 module.exports = UserStorage;
--- app/src/models/users.js
+++ app/src/models/users.js
@@ -10,6 +10,7 @@ class User {
 
     async login(){
         const client = this.body;
+        try {
         const user = await UserStorage.getUsersInfo(client.id);
         if(user) {
           if (user.id === client.id && user.password === client.password) {
@@ -18,8 +19,11 @@ class User {
         return { success: false, message: "비밀번호가 틀렸습니다."}
     }
     return { success: false, message: "존재하지 않는 아이디입니다."};
+  } catch (err) {
+    return { success: false, message: err };
   }
-  
+}
+
   async register() {
     const client = this.body;
     try {
