6b5bb245781ce467921c388f84e8e32f6eabce37
leesanghoon94 <leesanghoon94@github.com>
2023-08-24 01:23:53 +0900

    morgan 로그관리 구현

--- app/app.js
+++ app/app.js
@@ -3,11 +3,12 @@
 //모듈
 const express = require("express");
 const dotenv = require("dotenv");
-dotenv.config();
+const morgan = require("morgan");
+
 const app = express();
+dotenv.config();
 
-
-
+const accessLogStream = require("./src/config/log")
 //라우팅
 const home = require("./src/routes/home");
 
@@ -16,7 +17,8 @@ app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
 //use -> 미들웨어를 등록해주는 메서드.
-
+app.use(morgan("dev"));
+app.use(morgan("common", { stream: accessLogStream }));
 app.use(express.static(`${__dirname}/src/public`))
 app.use(express.json());
 //url을 통해 전달되는 데이터에 한글, 공백 등과 같은 문자가 포함될 경우 제대로 인식되지 않는 문제 해결
